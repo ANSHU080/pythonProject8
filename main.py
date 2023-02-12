736022385665325718d1b87688123e6bb1fd2905
Anshu Kumar <ansh23m@hotmail.com>
2023-02-12 21:25:29 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import requests
+app = Flask(__name__)
+
+@app.route('/cust-add/<id>')
+
+def cust_add(id):
+    link = "https://f3e3b133-50ee-40c1-ab73-e84d69ac7c58.mock.pstmn.io/cust-add"
+    f = requests.get(link)
+    a = (f.json())
+    for i in range(len(a)):
+        for key in a[i]:
+            if key == "addressID" and str(a[i][key])==id:
+                return a[i]
+
+if __name__ == '__main__':
+    app.run()
