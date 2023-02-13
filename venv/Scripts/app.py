3e82a2c585cf6516db09a30d09226d751ecebf23
Anshu Kumar <ansh23m@hotmail.com>
2023-02-13 20:27:25 +0530

    changes in app.py

--- venv/Scripts/app.py
+++ venv/Scripts/app.py
@@ -2,16 +2,17 @@ from flask import Flask
 import requests
 app = Flask(__name__)
 
-@app.route('/cust-add/<id>')
+@app.route('/cust-add')
 
-def cust_add(id):
-    link = "https://f3e3b133-50ee-40c1-ab73-e84d69ac7c58.mock.pstmn.io/cust-add"
-    f = requests.get(link)
-    a = (f.json())
-    for i in range(len(a)):
-        for key in a[i]:
-            if key == "addressID" and str(a[i][key])==id:
-                return a[i]
+def cust_add():
+    # link = "https://f3e3b133-50ee-40c1-ab73-e84d69ac7c58.mock.pstmn.io/cust-add"
+    # f = requests.get(link)
+    # a = (f.json())
+    # for i in range(len(a)):
+    #     for key in a[i]:
+    #         if key == "addressID" and str(a[i][key])==id:
+    #             return a[i]
+    return "hello world"
 
 if __name__ == '__main__':
     app.run()
